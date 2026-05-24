5b735a9fad7e0b55e65333ec071bf1fcdcc9f644
Simone Mariotti <simone@mariotti.me>
2026-05-24 16:41:33 +0200

    feat: add BellIcon component to ShareIcon

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -296,6 +296,7 @@ function ShareIcon() {
 }
 
 
+function BellIcon() {
   return (
     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
       <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
